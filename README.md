9d52a374d68be377b2d4e3951c5e1f41d5da3be4
Jack Perry <125957065+TheExParrot@users.noreply.github.com>
2023-12-09 20:26:03 +1100

    Setup dependencies

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # react-weather-app
 React Front-End Weather Application
+
+## How to Run
+1. Install node.js
+2. Run cd weatherapp
+3. Run npm install react-router-dom@^6.20.1
+4. Run npm start
--- weatherapp/src/App.js
+++ weatherapp/src/App.js
@@ -1,20 +1,10 @@
-import './App.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        Hello World!
       </header>
     </div>
   );
